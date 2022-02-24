230af5634518b83a37423d24210e3385e312d660
ArifIBN <99979583+ArifIBN@users.noreply.github.com>
2022-02-24 15:58:52 +0000

    creating user schema & tables

---
+++ schema/user_registry.sql
@@ -0,0 +1,22 @@
+create schema user_registry;
+
+DROP TABLE IF EXISTS user_registry.user_type CASCADE;
+
+CREATE TABLE user_registry.user_type (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS user_registry.users CASCADE;
+
+CREATE TABLE user_registry.user (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    email varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    type_id integer NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_type
+        FOREIGN KEY(type_id)
+            REFERENCES user_registry.user_type(id)
+);
