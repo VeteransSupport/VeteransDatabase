9e7aea68a6e5c541331e93cb517811654ff1bafe
ArifIBN <99979583+ArifIBN@users.noreply.github.com>
2022-02-24 15:45:20 +0000

    user inserts

---
+++ insert/user_registry.sql
@@ -0,0 +1,5 @@
+INSERT INTO users.users_type (id, type) values (1, 'appAdmin');
+INSERT INTO users.users_type (id, type) values (2, 'appSupport');
+INSERT INTO users.users_type (id, type) values (3, 'charityAdmin');
+INSERT INTO users.users_type (id, type) values (4, 'charitySupport');
+INSERT INTO users.users_type (id, type) values (5, 'veteran');
