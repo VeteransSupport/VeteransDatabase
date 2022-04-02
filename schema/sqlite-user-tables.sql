a3ff502ebd239c1570b227534587993d2efad8f4
PervaizAhmad <60514729+PervaizAhmad@users.noreply.github.com>
2022-04-02 16:12:52 +0100

    creating new sqlite db file

---
+++ schema/sqlite-user-tables.sql
@@ -0,0 +1,31 @@
+DROP TABLE user_type;
+
+CREATE TABLE "user_type" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+DROP TABLE user;
+
+CREATE TABLE "user" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"email"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	"type_id"	INTEGER NOT NULL DEFAULT 5,
+	"charity_id"	INTEGER NOT NULL DEFAULT 1,
+	FOREIGN KEY("type_id") REFERENCES "user_type"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+DROP TABLE charities;
+
+CREATE TABLE "charities" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"title"	TEXT NOT NULL,
+	"description"	TEXT NOT NULL,
+	"image"	BLOB,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
