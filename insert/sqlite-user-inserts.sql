294e21eb7e18af09f1c179a503495b9d78142276
PervaizAhmad <60514729+PervaizAhmad@users.noreply.github.com>
2022-04-02 16:23:43 +0100

    new sqlite db insertsfile

---
+++ insert/sqlite-user-inserts.sql
@@ -0,0 +1,6 @@
+-- user_type
+INSERT INTO user_type (name) values ('appAdmin');
+INSERT INTO user_type (name) values ('appSupport');
+INSERT INTO user_type (name) values ('charityAdmin');
+INSERT INTO user_type (name) values ('charitySupport');
+INSERT INTO user_type (name) values ('veteran');
