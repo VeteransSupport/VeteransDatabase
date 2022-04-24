c5016ecfc36b1233bb32af0d722a44fefb2cab91
s-vasili <99969802+s-vasili@users.noreply.github.com>
2022-04-24 18:52:57 +0100

    S-updated sqlite-user-inserts

--- insert/sqlite-user-inserts.sql
+++ insert/sqlite-user-inserts.sql
@@ -4,3 +4,10 @@ INSERT INTO user_type (name) values ('appSupport');
 INSERT INTO user_type (name) values ('charityAdmin');
 INSERT INTO user_type (name) values ('charitySupport');
 INSERT INTO user_type (name) values ('veteran');
+
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q1. Do you feel tense, nervous or insecure?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q2. Do you feel down or are you lacking energy?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q3. Do you have more negative than positive thoughts about yourself?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q4. Have people noticed that you have become more irritable or aggressive?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q5. Have you been having more disturbed sleep/nightmares or trouble falling asleep?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q6. Have you had any feelings of, or attempted, self harm or suicide?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
