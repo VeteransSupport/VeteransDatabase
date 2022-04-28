670a0767839b5031112c0a5ec49508109123953c
PervaizAhmad <60514729+PervaizAhmad@users.noreply.github.com>
2022-04-28 16:14:34 +0100

    adding charities

--- insert/sqlite-user-inserts.sql
+++ insert/sqlite-user-inserts.sql
@@ -11,3 +11,9 @@ INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q3. Do you have more nega
 INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q4. Have people noticed that you have become more irritable or aggressive?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
 INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q5. Have you been having more disturbed sleep/nightmares or trouble falling asleep?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
 INSERT INTO quiz (question, mcq1, mcq2, mcq3) values ('Q6. Have you had any feelings of, or attempted, self harm or suicide?', 'Not at all', 'For a few days no', 'For more than 2 weeks');
+
+INSERT INTO charities (title, description) VALUES ('Rock2Recovery', 'Nationwide. Neuro Linguistic Programming (NLP) and integrated eye movement therapy (IEMT) with some hypnotherapy.');
+INSERT INTO charities (title, description) VALUES ('All Call Signs', 'Nationwide. Peer support, app based, volunteers, beacon alerts.');
+INSERT INTO charities (title, description) VALUES ('Phoenix Heroes', 'Nationwide. BACP counselling, employment and training.  Essex. carp angling and other outdoor activities.');
+INSERT INTO charities (title, description) VALUES ('Veterans at ease', 'North East. NLP therapy.');
+INSERT INTO charities (title, description) VALUES ('Veterans in Crisis', 'Sunderland. Dog therapy. Fitness clubs. SID project. Mental health training. Outdoor activities.');
